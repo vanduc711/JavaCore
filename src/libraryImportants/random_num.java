07df4d223c5f4c434ba6ede1ebf00ff96005b38f
vanduc711 <89618392+vanduc711@users.noreply.github.com>
2023-12-06 09:40:20 +0700

    Update random_num.java

--- src/libraryImportants/random_num.java
+++ src/libraryImportants/random_num.java
@@ -7,10 +7,6 @@ public class random_num {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random rd = new Random();
-		
-		int number = rd.nextInt();
-		System.out.println(number);
 		
 		StringBuilder stringBuilder = new StringBuilder();
 		
